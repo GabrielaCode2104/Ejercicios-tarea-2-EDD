dc4d33037c0aef3f900ef504a977361a7dfdbbdb
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-19 13:34:52 -0500

    .

---
+++ Ejercicio 01/Ejercicio 01/Program.cs
@@ -0,0 +1,21 @@
+﻿//#Ejercicio 1: Escribe una función recursiva que imprima los números pares del 1 al 100. 
+public static class Recursividad
+{
+    public static void ImprimirPares(int num)
+    {
+        if (num <= 100)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num);
+            }
+            ImprimirPares(num + 1);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        ImprimirPares(1);
+    }
+}
+
---
+++ Ejercicio 01/Ejercicio 02- recursividad/Program.cs
@@ -0,0 +1,24 @@
+﻿// #2
+public static class Recursividad
+{
+    public static int ImprSuma(int n)
+    {
+        if (n > 0)
+        {
+            int suma = ImprSuma(n - 1) + n;
+            Console.WriteLine(n);
+            return suma;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int suma = ImprSuma(10);
+        Console.WriteLine("La suma es: " + suma);
+    }
+}
+
+
