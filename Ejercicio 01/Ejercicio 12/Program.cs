371934bb3f08b2a3e890d2f01c60eac5db315c4e
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-19 16:28:05 -0500

    12

---
+++ Ejercicio 01/Ejercicio 12/Program.cs
@@ -0,0 +1,30 @@
+﻿// 12) Calcula la media de los elementos de una matriz. 
+class Programa_12
+{
+    static void Main()
+    {
+        int[,] matriz = new int[3, 3] { { 50, 20, 10 }, { 30, 35, 40 }, { 55, 60, 75 } };
+
+        double media = CalcularMediaMatriz(matriz);
+        Console.WriteLine("Media de los elementos de la matriz: " + media);
+    }
+
+    static double CalcularMediaMatriz(int[,] matriz)
+    {
+        int suma = 0;
+
+        int elementos = matriz.GetLength(0) * matriz.GetLength(1);
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                suma += matriz[i, j];
+            }
+        }
+        double media = (double)suma / elementos;
+        return media;
+    }
+}
+
+
