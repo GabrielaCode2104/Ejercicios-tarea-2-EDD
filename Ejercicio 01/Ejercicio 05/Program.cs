da6b82ec0b71253459ec3875518b6eaa9375e690
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-19 15:05:25 -0500

    5

---
+++ Ejercicio 01/Ejercicio 05/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ejercicio 5: Escribe una función recursiva que imprima la tabla de multiplicar del n. 
+class Programa_5
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un número: ");
+        int num01 = Convert.ToInt32(Console.ReadLine());
+        ImprimirTablaMultiplicar(num01);
+    }
+    static void ImprimirTablaMultiplicar(int n, int multiplicador = 1)
+    {
+        if (multiplicador > 12)
+        {
+            return;
+        }
+        Console.WriteLine($"{n} x {multiplicador} = {n * multiplicador}");
+        ImprimirTablaMultiplicar(n, multiplicador + 1);
+    }
+}
