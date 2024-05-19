767816383752fd0ae1e53ac09c72dea8ad851798
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-19 16:07:32 -0500

    10

---
+++ Ejercicio 01/Ejercicio 10/Program.cs
@@ -0,0 +1,30 @@
+﻿// 10) Suma dos matrices de diferentes tamaños.
+class Programa_10
+{
+    static void Main()
+    {
+        int[,] matriz01 = new int[2, 2] { { 4, 3 }, { 5, 7 } };
+        int[,] matriz02 = new int[2, 2] { { 12, 6 }, { 14, 15 } }; 
+
+        int[,] resultado = new int[2, 2]; 
+
+        // Suma las matrices elemento a elemento
+        for (int fila = 0; fila < 2; fila++)
+        {
+            for (int columna = 0; columna < 2; columna++)
+            {
+                resultado[fila, columna] = matriz01[fila, columna] + matriz02[fila, columna]; 
+            }
+        }
+
+        Console.WriteLine("Suma de matrices:");
+        for (int fila = 0; fila < 2; fila++)
+        {
+            for (int columna = 0; columna < 2; columna++)
+            {
+                Console.Write(resultado[fila, columna] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
