a0c891a217e0fc7cfb5b5d15be6b4cf2877b91c9
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-19 22:42:36 -0500

    2 matrices

---
+++ Ejercicio 01/Ejercicio 2 . matrices/Program.cs
@@ -0,0 +1,28 @@
+﻿// Ejercicio 2: Calcula la media, la mediana y la desviación estándar de los elementos de una matriz.
+
+class EstadisticasMatriz
+{
+    static void Main()
+    {
+        int[,] matr = new int[3, 3] { { 11, 2, 31 }, { 40, 52, 6 }, { 73, 82, 19 } };
+
+        var elemen = matr.Cast<int>().ToList();
+
+        double media = elemen.Average();
+
+        // Calculamos la mediana
+        elemen.Sort();
+        double mediana = elemen.Count % 2 == 0 ?
+            (elemen[elemen.Count / 2 - 1] + elemen[elemen.Count / 2]) / 2.0 :
+            elemen[elemen.Count / 2];
+
+        // Calculamos la desviación estándar
+        double sumDifCuad = elemen.Sum(x => Math.Pow(x - media, 2));
+        double desvEstandar = Math.Sqrt(sumDifCuad / elemen.Count);
+
+        // Imprimimos los resultados
+        Console.WriteLine($"Media: {media}");
+        Console.WriteLine($"Mediana: {mediana}");
+        Console.WriteLine($"Desviación Estándar: {desvEstandar}");
+    }
+}
