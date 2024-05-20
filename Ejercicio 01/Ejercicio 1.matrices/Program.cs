ef3d46a3edaf9509474d69a28161766ac4d4e4a5
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-19 22:35:05 -0500

    1 matrices

---
+++ Ejercicio 01/Ejercicio 1.matrices/Program.cs
@@ -0,0 +1,26 @@
+﻿// #1 Crea una matriz de números aleatorios de tamaño 100x100.
+class GeneradorMatriz
+{
+    static void Main()
+    {
+        Random generadorAleatorio = new Random();
+        int[,] matrizAleatoria = new int[100, 100];
+        for (int fila = 0; fila < 100; fila++)
+        {
+            for (int columna = 0; columna < 100; columna++)
+            {
+                matrizAleatoria[fila, columna] = generadorAleatorio.Next(1, 101);
+
+            }      
+        }
+
+        for (int fila = 0; fila < 100; fila++)
+        {
+            for (int columna = 0; columna < 100; columna++)
+            {
+                Console.Write(matrizAleatoria[fila, columna] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
